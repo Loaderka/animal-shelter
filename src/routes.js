091c7d844fd5619fbdd487d3c0419e48c9312caf
Loaderka <60072743+Loaderka@users.noreply.github.com>
2021-04-18 01:56:57 +0300

    bundle

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+export const ROUTES = {
+    MAIN: {
+        INDEX: '/',
+    },
+    ANIMALS: {
+        INDEX: '/animals',
+        ITEM: '/animals/:id',
+    },
+    ASSISTANCE: {
+        INDEX: '/assistance',
+        TYPE: '/assistance/:assistanceType',
+    },
+    JOIN: {
+        INDEX: '/join',
+    },
+    CONTACTS: {
+        INDEX: '/contacts',
+    },
+    VOLONTEERS: {
+        INDEX: '/volunteers',
+        ITEM: '/volunteers/:id',
+    },
+    ABOUT: {
+        INDEX: '/about',  
+    },
+    HELP: {
+        INDEX: '/help',
+    },
+    TRANSFER: {
+        INDEX: '/transfer',
+    },
+    SELECTED: {
+        INDEX: '/selected',
+    },
+    FAQ: {
+        INDEX: '/faq',
+    },
+};
